65be7d6b2b91adee88b05aff60aa76a7157c13a5
delguoqing <delguoqing@hotmail.com>
2017-05-11 21:16:36 +0800

    implement jacobian pseduoinverse method

--- ik/gui.py
+++ ik/gui.py
@@ -1,13 +1,105 @@
+import random
+import cmath
 import pyglet
+import jacobian
+import numpy
 
 WIDTH = 640
 HEIGHT = 480
+HALFW = WIDTH / 2.0
+HALFH = HEIGHT / 2.0
 
+class IKChain(object):
+    
+    def __init__(self, l):
+        self.l = l
+        self.n = len(l)
+        self.theta = [0] * self.n
+        for i in xrange(self.n):
+            self.theta[i] = random.uniform(-0.001, 0.001)
+        self.jx = []
+        for i in xrange(self.n):
+            self.jx.append(self.l[i])
+            if i - 1 >= 0:
+                self.jx[-1] += self.jx[i - 1]
+        self.jy = [0] * self.n
+        self.solver = None
+    
+    def set_solver(self, solver):
+        self.solver = solver
+        
+    def solve(self, t, max_iter=10, epsilon=1e-3):
+        opt_theta, jx, jy = self.solver(self.l, self.theta, t, max_iter=max_iter, epsilon=epsilon)
+        self.jx = jx
+        self.jy = jy
+        self.theta = opt_theta
+       
 window = pyglet.window.Window(WIDTH, HEIGHT)
+
+ik_chain = None
+target_pos = None
+
+def draw_ik_chain(ik_chain):
+    jx = ik_chain.jx
+    jy = ik_chain.jy
+    pyglet.gl.glColor3f(0.0, 0.0, 1.0)
+    pyglet.gl.glLineWidth(2)
+    for i in xrange(ik_chain.n):
+        if i == 0:
+            draw_line((0, 0), (jx[i], jy[i]))
+        else:
+            draw_line((jx[i - 1], jy[i - 1]), (jx[i], jy[i]))
+    pyglet.gl.glColor3f(1.0, 1.0, 1.0)
+    pyglet.gl.glPointSize(5)
+    for i in xrange(ik_chain.n):
+        draw_point((jx[i], jy[i]))
+    draw_point((0, 0))
+    
+def draw_line(pos1, pos2):
+    pyglet.graphics.draw(2, pyglet.gl.GL_LINES, 
+            ("v2f", (pos1[0] + HALFW, pos1[1] + HALFH, pos2[0] + HALFW, pos2[1] + HALFH))
+        )
+
+def draw_point(pos):
+    pyglet.graphics.draw(1, pyglet.gl.GL_POINTS, 
+        ("v2f", (pos[0] + HALFW, pos[1] + HALFH))
+    )
 
 @window.event
 def on_draw():
-    pass
+    window.clear()
+    draw_ik_chain(ik_chain)
+    
+    if target_pos is not None:
+        pyglet.gl.glPointSize(10)
+        pyglet.gl.glColor3f(1.0, 0.0, 0.0)
+        draw_point(target_pos)
+
+def on_update(dt):
+    if target_pos is not None:
+        d = numpy.array([target_pos[0] - ik_chain.jx[-1], target_pos[1] - ik_chain.jy[-1]])
+        dlen = numpy.linalg.norm(d)
+        speed = 40.0
+        dist = speed * dt
+        if dist > dlen:
+            t = target_pos
+        else:
+            d = d / dlen * dist
+            t = (ik_chain.jx[-1] + d[0], ik_chain.jy[-1] + d[1])
+        ik_chain.solve(t)
+        
+@window.event
+def on_mouse_press(x, y, button, modifiers):
+    global target_pos
+    target_pos = (x - HALFW, y - HALFH)
 
 def start():
+    global ik_chain
+    ik_chain = IKChain([20, 20, 40, 60])
+    ik_chain.set_solver(jacobian.solve)
+    pyglet.clock.schedule_interval(on_update, 1.0 / 60.0)
+    
     pyglet.app.run()
+
+start()
+    
--- ik/jacobian.py
+++ ik/jacobian.py
@@ -1,11 +1,43 @@
 import numpy
 import cmath
-g_jx = None
-g_jy = None
 
 def solve(l, theta, t, max_iter=10, epsilon=1e-3):
-    pass
-
+    n = len(l)
+    jx = [0] * n
+    jy = [0] * n
+    opt_theta = numpy.matrix(theta).reshape(n, 1)
+    
+    step = 1.0
+    i = 0
+    while i < max_iter:
+        #print "step %d" % i
+        calc_world_positions(l, opt_theta.getA1(), jx, jy)
+        #print "positions:"
+        #print jx
+        #print jy
+        e = (t[0] - jx[-1], t[1] - jy[-1])
+        e = numpy.matrix([[e[0]], [e[1]]])
+        #print "error:"
+        #print numpy.linalg.norm(e)
+        if numpy.linalg.norm(e) < epsilon:
+            break
+        j = calc_jacobian(jx, jy)
+        j_val = calc_jacobian_numerical(lambda _theta, _jx, _jy: calc_world_positions(l, _theta, _jx, _jy), opt_theta.getA1(), 1e-2)
+        #print "jacobian"
+        #print j
+        #print "gradient checking"
+        #print j_val
+        jinv = numpy.linalg.pinv(j)
+        dtheta = jinv * e
+        #print "dtheta"
+        #print dtheta
+        #print "verify dtheta"
+        #print j * dtheta - e
+        opt_theta += step * dtheta
+        #print "new theta=", opt_theta
+        i += 1
+    return opt_theta.getA1(), jx, jy
+    
 def calc_world_positions(l, theta, jx, jy):
     world_mat = []
     n = len(l)
@@ -14,8 +46,8 @@ def calc_world_positions(l, theta, jx, jy):
         c = cmath.cos(theta[i])
         s = cmath.sin(theta[i])
         mat = numpy.matrix([
-            [c, s, l[i] * c],
-            [-s, c, -l[i] * s],
+            [c, -s, l[i] * c],
+            [s, c, l[i] * s],
             [0, 0, 1]
         ])
         if i == 0:
@@ -27,13 +59,64 @@ def calc_world_positions(l, theta, jx, jy):
         jx[i] = world_pos[0]
         jy[i] = world_pos[1]
 
-def calc_jacobian():
-    pass
+# returns: 2 x n jacobian matrix
+def calc_jacobian(jx, jy):
+    s = (jx[-1], jy[-1])
+    n = len(jx)
+    j = []
+    for i in xrange(n):
+        if i == 0:
+            x = s[0]
+            y = s[1]
+        else:
+            x = s[0] - jx[i - 1]
+            y = s[1] - jy[i - 1]
+        j.append(-y)
+        j.append(x)
+    j = numpy.matrix(j).reshape(n, 2).T
+    return j
 
+# sort of gradient checking
+def calc_jacobian_numerical(f, theta, epsilon):
+    j = []
+    n = len(theta)
+    pjx = [0] * n
+    pjy = [0] * n
+    njx = [0] * n
+    njy = [0] * n
+    for i in xrange(len(theta)):
+        ptheta = list(theta)
+        ptheta[i] += epsilon
+        f(ptheta, pjx, pjy)
+        ntheta = list(theta)
+        ntheta[i] -= epsilon
+        f(ntheta, njx, njy)
+        j.append((pjx[-1] - njx[-1]) / (2 * epsilon))
+        j.append((pjy[-1] - njy[-1]) / (2 * epsilon))
+    j = numpy.matrix(j).reshape(n, 2).T
+    return j
+        
 if __name__ == "__main__":
     lx = [0] * 4
     ly = [0] * 4
-    calc_world_positions([1, 1, 2, 3], [cmath.pi / 6.0, -cmath.pi / 6.0, cmath.pi / 2.0, -cmath.pi / 2.0], lx, ly)
+    l = [1, 1, 2, 3]
+    t = (0, 4)
+    theta = [cmath.pi / 6.0, -cmath.pi / 6.0, cmath.pi / 2.0, -cmath.pi / 2.0]
+    calc_world_positions(l, theta, lx, ly)
     for i in xrange(4):
         # print "%.2f, %.2f" % (lx[i], ly[i])
         print lx[i], ly[i]
+    j = calc_jacobian(lx, ly)
+    print j
+    print "pseudo inverse of Jacobian:"
+    print numpy.linalg.pinv(j)
+    print "start solving ik:"
+    opt_theta, jx, jy = solve(l, theta, t, max_iter=10, epsilon=1e-2)
+    print "final positions:"
+    print jx
+    print jy
+    print "verify theta:"
+    calc_world_positions(l, opt_theta, lx, ly)
+    for i in xrange(4):
+        # print "%.2f, %.2f" % (lx[i], ly[i])
+        print lx[i], ly[i]    
